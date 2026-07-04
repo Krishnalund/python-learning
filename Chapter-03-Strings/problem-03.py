76e4841e7e303d63255c540069c69d3c33343785
Krishnalund <krishnalund026@gmail.com>
2026-07-05 03:22:37 +0500

    Complete Chapter 03 - Strings

---
+++ Chapter-03-Strings/problem-03.py
@@ -0,0 +1,4 @@
+# Write a program to detect double space in a string
+course='Python  Learning'
+double_space=course.find("  ")
+print('Does the string contain double space:',double_space)
---
+++ Chapter-03-Strings/problem-04.py
@@ -0,0 +1,4 @@
+# Replace the double space from problem 3 with single spaces.
+course='Python  Learning'
+single_space=course.replace("  "," ")
+print(single_space)
